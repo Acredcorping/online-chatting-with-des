074bf00e96626cf5a49fe763d757c06da8739eb3
dp <104316695+Acredcorping@users.noreply.github.com>
2023-05-25 00:48:10 +0800

    Delete desProject2 directory

--- desProject2/chat/utils/secret_key_generator.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-
-def generate_key(length):
-    alphabet = string.ascii_letters + string.digits
-    key = ''.join(secrets.choice(alphabet) for _ in range(length))
-    return key
--- desProject2/desProject2/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from chat import consumers
-
-websocket_urlpatterns = [
-    re_path(r'chat/(?P<userid>\w+)&(?P<objid>\w+)/$', consumers.ChatConsumer.as_asgi()),
-]
